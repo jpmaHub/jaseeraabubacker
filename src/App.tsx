bc42962e963fd99695f669952a56999a8d647b29
“Jaseera <j.p.mohamedabubacker@gmail.com>
2024-06-03 21:41:12 +0100

    Use BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 
 import AboutMe from './pages/Projects';
 import Home from './pages/Home';
-import { Route, Routes, HashRouter } from 'react-router-dom'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={"/jaseeraabubacker"}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-me" element={<AboutMe />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 

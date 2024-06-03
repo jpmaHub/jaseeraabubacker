8026ecb095ab609f1035f86ec2190385254c79e7
“Jaseera <j.p.mohamedabubacker@gmail.com>
2024-06-03 11:52:06 +0100

    Move svg logo into own folder dir; clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import reactLogo from './react-logo.svg';
-import tsLogo from './ts-logo.svg';
-import jsLogo from './js-logo.svg';
-import rubyLogo from './ruby-logo.svg';
-import cwLogo from './code-wars.svg';
-import awsLogo from './aws-logo.svg';
-import terraformLogo from './te-logo.svg';
-import './App.css';
+import ReactLogo from './logo/react-logo.svg';
+import TypeScriptLogo from './logo/ts-logo.svg';
+import JavaScriptLogo from './logo/js-logo.svg';
+import RubyLogo from './logo/ruby-logo.svg';
+import CodeWarsLogo from './logo/code-wars.svg';
+import AWSLogo from './logo/aws-logo.svg';
+import TerraformLogo from './logo/te-logo.svg';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { Typography } from '@mui/material'
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import Divider from '@mui/material/Divider';
+import { Typography } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './App.scss';
+
 const theme = createTheme();
 
 theme.typography.h5 = {
@@ -22,35 +22,39 @@ theme.typography.h5 = {
 function App() {
   return (
     <div className="grid-container">
-      <div className="grid-item-logo">
+      <div className="grid-item grid-item-logo">
         <LinkedInIcon sx={{ fontSize: 50 }} />
         <GitHubIcon sx={{ fontSize: 50 }} />
-        <img className="code-wars-logo" src={cwLogo} alt="cwlogo" />
+        <img className="code-wars-logo" src={CodeWarsLogo} alt="CodeWarsLogo" />
       </div>
-      <div className="grid-item-name">
+      
+      <div className="grid-item grid-item-name">
         <Typography variant="h2" sx={{ fontWeight: 'bold', fontFamily: 'American Typewriter' }}>Jaseera Abubacker . . /</Typography>
       </div>
 
-      <div className="grid-item-line">
-         <Divider variant="inset" sx={{ fontSize: '80px', bgcolor: 'lightseagreen'}}/>
-      </div>
-
-      <div className="grid-item-list">
+      <div className="grid-item grid-item-list">
         <ThemeProvider theme={theme}>
           <Typography variant="h5">Projects</Typography>
           <Typography variant="h5">Experience</Typography>
           <Typography variant="h5">About Me</Typography>
         </ThemeProvider>
       </div>
-      
-      <div className="grid-item-text">
-        <img className="js-logo" src={jsLogo} alt="js-logo" />
-        <img className="aws-logo" src={awsLogo} alt="aws-logo" />
-        <img className="terraform-logo" src={terraformLogo} alt="terraform-logo" />
-        <Typography variant="h5" sx={{ fontFamily: 'Trebuchet MS' }}>Hello, I am Jaseera! I'm a full stack developer who loves creating applications. Let's build something great together!</Typography>
-        <img className="ruby-logo" src={rubyLogo} alt="ruby-logo" />
-        <img className="react-logo" src={reactLogo} alt="react-logo" />
-        <img className="ts-logo" src={tsLogo} alt="ts-logo" />
+
+      <div className="grid-item grid-item-intro">
+        <div className="grid-item grid-item-text">
+          <Typography className="about-me" variant="h5" sx={{ fontFamily: 'Trebuchet MS' }}>Hello, I am Jaseera! I'm a full stack developer who loves creating applications. Let's build something great together!</Typography>
+        </div>
+        <div className="grid-item-line">
+         <Divider sx={{ fontSize: '80px', bgcolor: 'lightseagreen'}}/>
+      </div>
+        <div className="grid-item grid-item-skills-logo">
+          <img className="js-logo" src={JavaScriptLogo} alt="js-logo" />
+          <img className="aws-logo" src={AWSLogo} alt="aws-logo" />
+          <img className="terraform-logo" src={TerraformLogo} alt="terraform-logo" />
+          <img className="ruby-logo" src={RubyLogo} alt="ruby-logo" />
+          <img className="react-logo" src={ReactLogo} alt="react-logo" />
+          <img className="ts-logo" src={TypeScriptLogo} alt="ts-logo" />
+        </div>
       </div>
     </div>
   );

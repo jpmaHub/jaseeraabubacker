0b1216411e2f3aec69ffdf3c1a4674216e49454f
“Jaseera <j.p.mohamedabubacker@gmail.com>
2024-06-03 15:48:38 +0100

    Use HashRouter for github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 
 import AboutMe from './pages/AboutMe';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes, HashRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-me" element={<AboutMe />} />
       </Routes>
-    </Router>
+    </HashRouter>
   )
 }
 

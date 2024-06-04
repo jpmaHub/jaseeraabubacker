26dde2d631649ccaf27999908a6f5ea7dc8ba621
“Jaseera <j.p.mohamedabubacker@gmail.com>
2024-06-04 16:47:31 +0100

    Add email icon

--- src/components/ContactIcons.tsx
+++ src/components/ContactIcons.tsx
@@ -1,20 +1,25 @@
 import CodeWarsLogo from '../assets/code-wars.svg';
 import GitHubLogo from '../assets/github.svg';
 import LinkedInLogo from '../assets/linkedin.svg';
+import EmailLogo from '../assets/email.svg';
+import EmailIcon from '@mui/icons-material/Email';
+import { Link } from '@mui/material';
 
 export default function ContactIcons() {
     return (
         <>
-            <a href="https://github.com/jpmaHub">
+            <Link href="https://github.com/jpmaHub">
                 <img className="github-logo" src={GitHubLogo} alt="GitHubLogo" />
-            </a>
-            <a href="https://www.linkedin.com/in/mjaseera">
+            </Link>
+            <Link href="https://www.linkedin.com/in/mjaseera">
                 <img className="linkedin-logo" src={LinkedInLogo} alt="LinkedInLogo" />
-            </a>
-            <a href="https://www.codewars.com/users/jpmaHub">
+            </Link>
+            <Link href="mailto:someone@email.com">
+                <img className="email-logo" src={EmailLogo} alt="EmailLogo" />
+            </Link>
+            <Link href="https://www.codewars.com/users/jpmaHub">
                 <img className="code-wars-logo" src={CodeWarsLogo} alt="CodeWarsLogo" />
-            </a>
-
+            </Link>
         </>
     )
 }

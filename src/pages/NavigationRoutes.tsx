5b1fecf02402c10e2e01f36022136382edce21da
“Jaseera <j.p.mohamedabubacker@gmail.com>
2024-06-04 16:48:31 +0100

    Add new page

--- src/pages/NavigationRoutes.tsx
+++ src/pages/NavigationRoutes.tsx
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Projects from './Projects';
 import Experience from './Experience';
+import References from './References';
 
 export function NavigationRoutes() {
   return (
@@ -10,6 +11,7 @@ export function NavigationRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/my-projects" element={<Projects />} />
         <Route path="/my-experience" element={<Experience />} />
+        <Route path="/references" element={<References />} />
       </Routes>
   )
 }
